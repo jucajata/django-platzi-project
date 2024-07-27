9d337c853c3b9d18631308322bdb10b6b39cc21c
jucajata <jcjt95@gmail.com>
2024-07-27 11:58:22 -0500

    feat: nueva app orders

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'crispy_tailwind',
     'products',
     'users',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'
 
 TIME_ZONE = 'UTC'
 
@@ -133,3 +134,4 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 CRISPY_TEMPLATE_PACK = "tailwind"
 
 LOGIN_REDIRECT_URL = 'list_product'
+LOGOUT_REDIRECT_URL = 'list_product'
